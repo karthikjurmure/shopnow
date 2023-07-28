dad443f2803229c01da245d823e29f9e1a9eaf33
karthikjurmure <karthikjurmure@gmail.com>
2023-07-28 12:23:38 +0530

    first commit

--- src/products.js
+++ src/products.js
@@ -1,6 +1,6 @@
-import product1 from "./assets/products/1.png";
+//import product1 from "./assets/products/1.jpeg";
 import product2 from "./assets/products/2.png";
-import product3 from "./assets/products/3.png";
+//import product3 from "./assets/products/3.jpeg";
 import product4 from "./assets/products/4.png";
 import product5 from "./assets/products/5.png";
 import product6 from "./assets/products/6.webp";
@@ -8,24 +8,24 @@ import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
 
 export const PRODUCTS = [
-  {
+  /*{
     id: 1,
     productName: "jacket",
     price: 999.0,
     productImage: product1,
-  },
+  },*/
   {
     id: 2,
     productName: "jeans",
     price: 1999.0,
     productImage: product2,
   },
-  {
+  /*{
     id: 3,
     productName: "shirt",
     price: 699.0,
     productImage: product3,
-  },
+  },*/
   {
     id: 4,
     productName: "WLS Van Gogh Denim Jacket",
